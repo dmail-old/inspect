e65de5e5f2adc2127d1c693d692135ee14691c0c
Damien Maillard <damien.maillard@dailymotion.com>
2019-07-10 12:58:42 +0200

    update to latest dev depdendencies

--- script/start-exploring-server/start-exploring-server.js
+++ script/start-exploring-server/start-exploring-server.js
@@ -1,7 +1,7 @@
-const { startBrowserExplorerServer } = require("@jsenv/core")
+const { startExploringServer } = require("@jsenv/core")
 const { projectPath } = require("../../jsenv.config.js")
 
-startBrowserExplorerServer({
+startExploringServer({
   projectPath,
   port: 3456,
   forcePort: true,
